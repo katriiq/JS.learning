ca349e1a752245aa51c4c4bd6fc7905bfdda3c87
katriiq <petrova.yekaterina.1@mail.ru>
2026-01-13 19:44:57 +0300

    13.01

---
+++ typesofvar.js
@@ -0,0 +1,70 @@
+//строка
+
+let text = 'это строка'
+
+console.log('text'
+            , 'тип:',
+            typeof text,
+            'значение:',
+            text)
+
+//числа
+let age = 24 
+console.log('age'
+            , 'тип:',
+            typeof age,
+            'значение:',
+            age)
+
+let inf = Infinity 
+console.log('inf'
+            , 'тип:',
+            typeof inf,
+            'значение:',
+            inf)
+
+let notANumber = NaN
+console.log('notANumber'
+            , 'тип:',
+            typeof notANumber,
+            'значение:',
+            notANumber)
+
+//булевый тип
+let itIsTrue = true
+console.log('itIsTrue'
+            , 'тип:',
+            typeof itIsTrue,
+            'значение:',
+            itIsTrue)
+
+let itIsFalse = true
+console.log('itIsFalse'
+            , 'тип:',
+            typeof itIsFalse,
+            'значение:',
+            itIsFalse)
+
+//большое число
+let countOfStars = 99999999999999999999999999999999999999999999999999
+console.log('countOfStars'
+            , 'тип:',
+            typeof countOfStars,
+            'значение:',
+            countOfStars)
+
+//undefined
+let unknown
+console.log('unknown'
+            , 'тип:',
+            typeof unknown,
+            'значение:',
+            unknown)
+
+// Object
+let human = {}
+console.log('human'
+            , 'тип:',
+            typeof human,
+            'значение:',
+            human)
