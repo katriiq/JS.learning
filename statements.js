300abb8ef19470bd90d0f12abd098257cb6a6cdc
katriiq <petrova.yekaterina.1@mail.ru>
2026-01-20 19:03:06 +0300

    20/01

---
+++ statements.js
@@ -0,0 +1,14 @@
+//операторы в JS
+
+//унарные операторы
+let positiveNan = +4
+let negativeNan = -4
+
+let count = "45"
+let countAsNum = +count
+// Number(count) ==== +count
+console.log(typeof countAsNum, countAsNum)
+
+let isHide = "yes"
+let isHideBool = !isHide
+console.log(typeof isHideBool, isHideBool)
